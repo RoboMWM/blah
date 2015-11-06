62f5c4d23df823a1a9f3e8267ce21b08020333ee
RoboMWM <robomwm@users.noreply.github.com>
2015-11-06 03:23:22 -0800

    Thanks to TheUnnamedDude and CrystalMare in #spigot

--- src/me/robomwm/blah/ChatListener.java
+++ src/me/robomwm/blah/ChatListener.java
@@ -1,6 +1,7 @@
 package me.robomwm.blah;
 
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
@@ -21,12 +22,35 @@ public class ChatListener implements Listener
         String command = args[0].toLowerCase();
         if (ds.isSoftMuted((event.getPlayer().getUniqueId())))
         {
-            if (command.equals("/me") || command.equals("/tell") || command.equals("/msg") || command.equals("/w") || command.equals("/t") || command.equals("/pm"))
+            if (command.equals("/tell") || command.equals("/msg") || command.equals("/w") || command.equals("/t") || command.equals("/pm"))
             {
-                event.setCancelled(true);
+                {
+                    if (args.length > 2)
+                    {
+                        if (getServer().getPlayerExact(args[1]) != null && getServer().getPlayerExact(args[1]) != event.getPlayer())
+                        {
+                            String pm = ":";
+                            for (int i = 2; i < args.length; i++)
+                                pm += (" " + args[i]);
+                            //If you know a better way to do the formatting below, please let me know, thanks :)
+                            event.getPlayer().sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "You whisper to " + getServer().getPlayer(args[1]).getName() + pm);
+                            event.setCancelled(true);
+                        }
+                    }
+                }
+            }
+            else if (command.equals("/me"))
+            {
+                if (args.length > 1)
+                {
+                    String pm = "";
+                    for (int i = 1; i < args.length; i++)
+                        pm += (" " + args[i]);
+                    event.getPlayer().sendMessage("* " + event.getPlayer().getName() + pm);
+                    event.setCancelled(true);
+                }
             }
         }
-
     }
 
 }
